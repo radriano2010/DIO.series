e87520d7401bb1f7bd26c028f65632c2de623338
Ricardo Roland <radriano2010@gmail.com>
2021-04-02 12:42:28 -0300

    Atualização final

---
+++ Classes/EntidadeBase.cs
@@ -0,0 +1,7 @@
+namespace DIO.Series
+{
+    public class EntidadeBase
+    {
+        
+    }
+}
---
+++ Classes/Serie.cs
@@ -0,0 +1,55 @@
+using System;
+
+namespace DIO.Series
+{
+    public class Serie : EntidadeBase
+    {
+        // Atributos
+		private Genero Genero { get; set; }
+		private string Titulo { get; set; }
+		private string Descricao { get; set; }
+		private int Ano { get; set; }
+        private bool Excluido {get; set;}
+		private int Ident {get; set;}
+
+        // Métodos
+		public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+		{
+			this.Ident = id;
+			this.Genero = genero;
+			this.Titulo = titulo;
+			this.Descricao = descricao;
+			this.Ano = ano;
+            this.Excluido = false;
+		}
+
+        public override string ToString()
+		{
+			
+            string retorno = "";
+            retorno += "Gênero: " + this.Genero + Environment.NewLine;
+            retorno += "Titulo: " + this.Titulo + Environment.NewLine;
+            retorno += "Descrição: " + this.Descricao + Environment.NewLine;
+            retorno += "Ano de Início: " + this.Ano + Environment.NewLine;
+            retorno += "Excluido: " + this.Excluido;
+			return retorno;
+		}
+
+        public string retornaTitulo()
+		{
+			return this.Titulo;
+		}
+
+		public int retornaIdent()
+		{
+			return this.Ident;
+		}
+        public bool retornaExcluido()
+		{
+			return this.Excluido;
+		}
+        public void Excluir() {
+            this.Excluido = true;
+        }
+    }
+}
